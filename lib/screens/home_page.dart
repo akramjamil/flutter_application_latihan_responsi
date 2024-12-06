8e9d8c3e7623732d794a41d20c9d491f467f26e5
akramjamil <akramjamil18@gmail.com>
2024-12-06 07:11:07 +0700

    first commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'data_page.dart';
+import 'login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUsername();
+  }
+
+  Future<void> _loadUsername() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      username = prefs.getString('username') ?? 'User';
+    });
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Hallo $username!'),
+      backgroundColor: (Colors.blue),
+    ),
+    body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Text(
+            'Welcome, $username!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 16.0,
+              mainAxisSpacing: 16.0,
+              children: [
+                _buildMenuItem(context, 'News', Icons.newspaper, 'articles'),
+                _buildMenuItem(context, 'Blogs', Icons.book, 'blogs'),
+                _buildMenuItem(context, 'Reports', Icons.report, 'reports'),
+              ],
+            ),
+          ),
+          // Tombol Logout dipindahkan ke bawah
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: ElevatedButton(
+                onPressed: () async {
+                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  await prefs.clear();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.blue), // Set color of the text
+                ),
+                child: Text('Logout'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _buildMenuItem(
+    BuildContext context, String title, IconData icon, String category) {
+  return GestureDetector(
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DataPage(category: category),
+        ),
+      );
+    },
+    child: Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 60, color: Colors.blue),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    ),
+  );
+ }
+}
