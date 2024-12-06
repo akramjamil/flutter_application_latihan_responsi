3360df0fae2cb6f6ae3927e07d738d7062178656
akramjamil <akramjamil18@gmail.com>
2024-12-06 07:38:09 +0700

    api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,26 +1,26 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/news_model.dart'; // Import Model
+import '../models/news_model.dart'; 
 
 class ApiService {
-  // Mengambil daftar berita
+  
   static Future<List<News>> fetchList(String category) async {
     final response = await http.get(Uri.parse('https://api.spaceflightnewsapi.net/v4/$category'));
 
     if (response.statusCode == 200) {
       List<dynamic> results = json.decode(response.body)['results'];
-      return results.map((item) => News.fromJson(item)).toList();  // Parsing JSON ke model News
+      return results.map((item) => News.fromJson(item)).toList();  
     } else {
       throw Exception('Failed to load data');
     }
   }
 
-  // Mengambil detail berita
+  
   static Future<News> fetchDetail(String category, int id) async {
     final response = await http.get(Uri.parse('https://api.spaceflightnewsapi.net/v4/$category/$id'));
 
     if (response.statusCode == 200) {
-      return News.fromJson(json.decode(response.body)); // Parsing JSON ke model News
+      return News.fromJson(json.decode(response.body)); 
     } else {
       throw Exception('Failed to load detail');
     }
